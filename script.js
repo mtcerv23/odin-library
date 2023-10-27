5012a4ef95922712612a60baab3bb8f1cb4c9d8a
Matthew <mattylerc@gmail.com>
2023-10-27 16:10:55 -0700

    remote

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(book);
 }
 
+// check if the book is already in main.
 function displayBook() {
   myLibrary.forEach(book => {
     const newDiv = document.createElement("div");
@@ -23,18 +24,30 @@ function displayBook() {
       <p><b>Pages:</b> ${book.pages}</p>
       <p><b>Already read?</b> ${book.read}</p>`;
       main.append(newDiv);
+      alert(myLibrary);
   })
 }
 
 const modal = document.querySelector("[data-modal]");
 const addBook = document.getElementById('add-book');
-
+const cancel = document.getElementById('cancel');
+const submit = document.getElementById('submit');
 
 addBook.onclick = () => {
   modal.showModal()
 }
 
-const submit = document.getElementById('submit');
+function resetForm() {
+  document.getElementById('title').value = null;
+  document.getElementById('author').value = null;
+  document.getElementById('pages').value = null;
+  document.getElementById('read').checked = false;
+}
+
+cancel.addEventListener('click', () => {
+  resetForm();
+  modal.close();
+})
 
 submit.addEventListener("click", function(event) {
   event.preventDefault();
@@ -43,10 +56,10 @@ submit.addEventListener("click", function(event) {
   let pages = document.getElementById('pages').value;
   let read = document.getElementById('read').checked;
   if (read === false) {read = 'No'} else read = 'Yes';
+
   if (title != '' && author != '' && pages != '') {
     addBookToLibrary(title, author, pages, read);
-
-  displayBook();
-  modal.close();
+    displayBook();
+    modal.close();
   }}
 );
